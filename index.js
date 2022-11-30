fed735e71def982805ace15e699c0ae2432f81fe
Tasfiajahan <tania.develpers@gmail.com>
2022-11-30 16:29:37 +0600

    my 9th commit

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ async function run() {
     try {
         const productCollection = client.db("alexashop").collection('products');
         const categoryCollection = client.db("alexashop").collection('categories');
+        const orderCollection = client.db("alexashop").collection('orders');
+        const userCollection = client.db("alexashop").collection('users');
 
         app.get('/all/products', async (req, res) => {
             const query = {}
@@ -33,11 +35,104 @@ async function run() {
             res.send(categories);
         })
 
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const category = await categoryCollection.findOne(query);
+            res.send(category);
+        })
+
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product);
+        })
+
+        app.get('/latest/product', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query).sort({ _id: -1 }).limit(4);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+        app.get('/category/product/:id', async (req, res) => {
+            const category = req.params.id;
+            const query = { category: category };
+            const products = await productCollection.find(query).toArray();
+            res.send(products);
+        })
+
+        app.post('/store/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+        app.get('/my/orders/:email', async (req, res) => {
+
+            const email = req.params.email;
+            const query = { email: email };
+            const orders = await orderCollection.find(query).toArray();
+            res.send(orders);
+        })
+
+        app.post('/store/product', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
+
+        app.post('/store/user', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+
+        })
+
+
+        app.get('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await userCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
+        app.get('/user/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await userCollection.findOne(query);
+            res.send({ isSeller: user?.role === 'seller' });
+        })
+
+        app.get('/all/users/:role', async (req, res) => {
+            const role = req.params.role;
+            const query = { role: role };
+            const users = await userCollection.find(query).toArray();
+            res.send(users);
+
+        })
+
+        app.get('/user/normal/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await userCollection.findOne(query);
+            res.send({ isUser: user?.role === 'user' });
+        })
+
+        app.get('/seller/products/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { seller_email: email };
+            const products = await productCollection.find(query).toArray();
+            res.send(products);
+
         })
 
 
@@ -51,7 +146,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send(`${process.env.APP_NAME} server is running.....`);
+    res.send(`${process.env.APP_NAME} server is running`);
 })
 
 app.listen(port, () => {
